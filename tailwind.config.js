b097fa4a8e3aeb3975ac617a10d2c2f7bc1e0aa0
kKerem <kerem.er35@gmail.com>
2023-02-21 22:32:25 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      screens: {
+          'sm': '100%',
+          'md': '100%',
+          'lg': '800px',
+          'xl': '800px',
+          '2xl': '800px',
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
